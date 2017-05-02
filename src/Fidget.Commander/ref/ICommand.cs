25305fe9e7a7cf9a9f2dbdd0faf5d5b3e5583aab
Sean Terry <sean.terry@seaseducation.com>
2017-05-02 11:02:03 -0500

    Command handler interface

--- src/Fidget.Commander/ref/ICommand.cs
+++ src/Fidget.Commander/ref/ICommand.cs
@@ -6,7 +6,7 @@
     /// <typeparam name="TResult">Type of the command result.</typeparam>
     /// <remarks>This is a marker interface.</remarks>
 
-    public interface ICommand<TResult> {}
+    public interface ICommand<out TResult> {}
 
     /// <summary>
     /// Defines a command whose execution returns no result.
---
+++ src/Fidget.Commander/ref/ICommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fidget.Commander
+{
+    /// <summary>
+    /// Defines a handler for executing a command.
+    /// </summary>
+    /// <typeparam name="TCommand">Type of the command.</typeparam>
+    /// <typeparam name="TResult">Type of the command result.</typeparam>
+
+    public interface ICommandHandler<in TCommand,TResult> where TCommand : ICommand<TResult>
+    {
+        /// <summary>
+        /// Handles execution of the command and returns a result.
+        /// </summary>
+        /// <param name="command">Command to be executed.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>The command result.</returns>
+        
+        Task<TResult> Handle( TCommand command, CancellationToken cancellationToken );
+    }
+
+    /// <summary>
+    /// Defines a handler for executing a command that returns no result.
+    /// </summary>
+    /// <typeparam name="TCommand">Type of the command.</typeparam>
+    
+    public interface ICommandHandler<in TCommand> : ICommandHandler<TCommand,Unit> where TCommand : ICommand {}
+}
