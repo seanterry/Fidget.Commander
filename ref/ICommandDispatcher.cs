5107cd3420c4dda9469b79c94c19bcafa3d952eb
Sean Terry <sean.terry@seaseducation.com>
2017-05-02 16:52:01 -0500

    Dispatcher interface

---
+++ ref/ICommandDispatcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fidget.Commander
+{
+    /// <summary>
+    /// Defines a dispatcher for resolving and executing the handlers for commands.
+    /// </summary>
+
+    public interface ICommandDispatcher
+    {
+        /// <summary>
+        /// Locates and executes the handler for the given command.
+        /// </summary>
+        /// <typeparam name="TResult">Type of the command result.</typeparam>
+        /// <param name="command">Command whose handler to execute.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>The command result.</returns>
+        
+        Task<TResult> Execute<TResult>( ICommand<TResult> command, CancellationToken cancellationToken = default(CancellationToken) );
+    }
+}
