0966486d47bc32493a2af22f0c5e67fb73924452
Sean Terry <sean.terry@seaseducation.com>
2017-05-02 10:50:36 -0500

    Command interface

---
+++ src/Fidget.Commander/ref/ICommand.cs
@@ -0,0 +1,17 @@
+﻿namespace Fidget.Commander
+{
+    /// <summary>
+    /// Defines a command whose execution returns a result.
+    /// </summary>
+    /// <typeparam name="TResult">Type of the command result.</typeparam>
+    /// <remarks>This is a marker interface.</remarks>
+
+    public interface ICommand<TResult> {}
+
+    /// <summary>
+    /// Defines a command whose execution returns no result.
+    /// </summary>
+    /// <remarks>This is a marker interface.</remarks>
+    
+    public interface ICommand : ICommand<Unit> {}
+}
