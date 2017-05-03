d01b84eb8d5dbfef6a1daf4339cadd5a56c3af04
Sean Terry <sean.terry@seaseducation.com>
2017-05-03 16:16:06 -0500

    Command decorator interface

---
+++ ref/Dispatch/CommandDelegate.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fidget.Commander.Dispatch
+{
+    /// <summary>
+    /// Defines a delegate type that represents command execution.
+    /// </summary>
+    /// <typeparam name="TCommand">Type of the executing command.</typeparam>
+    /// <typeparam name="TResult">Type of the command result.</typeparam>
+    /// <param name="command">Command being executed.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>The command result.</returns>
+
+    public delegate Task<TResult> CommandDelegate<TCommand,TResult>( TCommand command, CancellationToken cancellationToken ) where TCommand : ICommand<TResult>;
+}
