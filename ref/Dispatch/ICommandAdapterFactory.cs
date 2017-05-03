e125372fa96b72ee586584b1879c0bb68227ade0
Sean Terry <sean.terry@seaseducation.com>
2017-05-03 06:07:24 -0500

    Adapter factory interface

---
+++ ref/Dispatch/ICommandAdapterFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace Fidget.Commander.Dispatch
+{
+    /// <summary>
+    /// Factory for generating command adapters.
+    /// </summary>
+    /// <remarks>This is an extensibility point for dependency injection.</remarks>
+
+    public interface ICommandAdapterFactory
+    {
+        /// <summary>
+        /// Returns the appropriate adapter for the given command.
+        /// </summary>
+        /// <typeparam name="TResult">Type of the command result.</typeparam>
+        /// <param name="command">Command whose adapter to return.</param>
+        
+        ICommandAdapter<TResult> For<TResult>( ICommand<TResult> command );
+    }
+}
