0210ffd854c9bec64b48cde8b8d63682da1d7e53
Sean Terry <seanterry@outlook.com>
2017-05-05 12:29:28 -0500

    README and structure map sample

--- samples/Fidget.Commander.Samples.StructureMap/Startup.cs
+++ samples/Fidget.Commander.Samples.StructureMap/Startup.cs
@@ -1,21 +1,16 @@
-# Fidget.Commander
+﻿using Fidget.Commander.Dispatch;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using StructureMap;
+using StructureMap.AspNetCore;
 
-This is a simple dispatcher for executing commands in a CQRS-ish manner. In Fidget, however, everything is a command. In my day job as both a database administrator and API developer, I've found that commands frequently end up needing to return a value, and queries frequently require auditing.
+namespace Fidget.Commander.Samples.StructureMap
+{
+    /// <summary>
+    /// Application startup object.
+    /// </summary>
 
-Consequently, Fidged has a unified interface for defining a commands, handlers, and decorators.
-
-## Basics
-Once you've added the `Fidget.Commander` package to your project, you'll want to familiarize yourself with the following interfaces:
-- `ICommand<TResult>` is a marker interface for the command itself.
-- `ICommandHandler<TCommand,TResult>` defines a handler for executing the command.
-- `ICommandDecorator<TCommand,TResult>` allows you to modify a handler's behavior without modifying the handler itself.
-- `ICommandDispatcher` is implemented for you, and true to its name, it dispatches commands to be executed by their handlers.
-
-To get started, you'll need to tell your favorite DI container how to find the implementations of those interfaces.
-
-### StructureMap
-Here's a simple registration example using `StructureMap.AspNetCore`, which is available in the code samples:
-```csharp
     public class Startup
     {
         /// <summary>
@@ -52,7 +47,6 @@ Here's a simple registration example using `StructureMap.AspNetCore`, which is a
 
         public void ConfigureContainer( Registry registry )
         {
-            // add all your handlers and decorators
             registry.Scan( scanner =>
             {
                 scanner.TheCallingAssembly();
@@ -60,7 +54,6 @@ Here's a simple registration example using `StructureMap.AspNetCore`, which is a
                 scanner.ConnectImplementationsToTypesClosing( typeof( ICommandDecorator<,> ) );
             });
             
-            // add the types that Fidget needs
             registry.For( typeof(ICommandAdapter<,>) ).Use( typeof(CommandAdapter<,>) );
             registry.For<ICommandAdapterFactory>().Use<CommandAdapterFactory>();
             registry.For<ICommandDispatcher>().Use<CommandDispatcher>();
@@ -76,4 +69,4 @@ Here's a simple registration example using `StructureMap.AspNetCore`, which is a
             app.UseMvc();
         }
     }
-```
+}
